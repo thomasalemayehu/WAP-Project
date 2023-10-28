6203a97c4a3d8351ae89ad3c76c4bb5084ead764
Thomas Alemayehu <thomas2alexmech@gmail.com>
2023-10-28 18:36:14 -0500

    Auth middleware removed from ATM

--- server/routes/account.routes.js
+++ server/routes/account.routes.js
@@ -3,24 +3,50 @@ const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/account.controller");
 
-router.post("/", accountController.createAccount);
-router.get("/:id", accountController.getAccountById);
-router.put("/:id", accountController.updateById);
+const authenticationMiddleware = require("../middleware/authentication.middleware");
+
+router.post("/", authenticationMiddleware, accountController.createAccount);
+router.get("/:id", authenticationMiddleware, accountController.getAccountById);
+router.put("/:id", authenticationMiddleware, accountController.updateById);
 router.delete("/:id", accountController.deleteById);
 
-router.post("/:id/withdraw", accountController.withdraw);
-router.post("/:id/deposit", accountController.deposit);
-router.get("/:id/transaction", accountController.getTransaction);
+router.post(
+  "/:id/withdraw",
+  authenticationMiddleware,
+  accountController.withdraw
+);
+router.post(
+  "/:id/deposit",
+  authenticationMiddleware,
+  accountController.deposit
+);
+router.get(
+  "/:id/transaction",
+  authenticationMiddleware,
+  accountController.getTransaction
+);
 router.get(
   "/:id/transaction/:transactionId",
+  authenticationMiddleware,
   accountController.getTransactionById
 );
 
-router.get("/:senderId/transfer/:receiverId", accountController.transferById);
+router.get(
+  "/:senderId/transfer/:receiverId",
+  authenticationMiddleware,
+  accountController.transferById
+);
 router.post("/:id/atm/generate", accountController.generateCard);
 router.post("/atm", accountController.withdrawATM);
-router.get("/:id/info", accountController.accountInfo);
-router.get("/:id/filter",accountController.filterTransaction);
-
+router.get(
+  "/:id/info",
+  authenticationMiddleware,
+  accountController.accountInfo
+);
+router.get(
+  "/:id/filter",
+  authenticationMiddleware,
+  accountController.filterTransaction
+);
 
 module.exports = router;
